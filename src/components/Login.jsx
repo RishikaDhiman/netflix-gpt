db9111498140d61eaa3fc6fa769c87f6d643843b
RishikaDhiman <rishikadhiman2003@gmail.com>
2024-04-21 17:04:23 +0530

    commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Header from './Header'
+import { checkValidData } from '../utlis/validate';
 
 const Login = () => {
 
   const [signIn, setSignIn] = useState(true);
 
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const [errorMessage, setErrorMessage] = useState(null);
+
   const handleClick=()=>{
     setSignIn(!signIn);
+  }
+
+  const handleButtonClicked=()=>{
+    // validate data
+    const res = checkValidData(email.current.value,password.current.value);
+    // console.log(res);
+    // console.log(email.current.value,password.current.value);
+    setErrorMessage(res);
   }
 
   return (
     <div className='login_page'>
       <Header/>
-        <form className='login-box'>
+        <form onSubmit={(e)=>e.preventDefault()} className='login-box'>
           <h1>{signIn?"Sign In":"Sign Up"}</h1>
           {!signIn && <div className='login-small-box'>
-            {/* <label>Full Name : </label> */}
             <input type="text" placeholder='Enter your full name'></input>
           </div>}
           <div className='login-small-box'>
-            {/* <label>Email : </label> */}
-            <input type="text" placeholder='Enter your email address'></input>
+            <input type="email" placeholder='Enter your email address' ref={email}></input>
           </div>
           <div className='login-small-box'>
-            {/* <label>Password : </label> */}
-            <input type="text" placeholder='Enter your password'></input>
+            <input type="password" placeholder='Enter your password' ref={password}></input>
           </div>
-          <button>{signIn?"Sign In":"Sign Up"}</button>
-          <p onClick={handleClick} style={{cursor:'pointer'}}>{signIn?"New to Netflix ? Sign Up":"Already have a account ? Sign In"}</p>
+          {errorMessage!=null&& <p style={{color: "rgb(165, 4, 4)"}}>{errorMessage}</p>}
+          <button onClick={handleButtonClicked}>{signIn?"Sign In":"Sign Up"}</button>
+          <p onClick={handleClick} style={{cursor:'pointer'}}>{signIn?"New to Netflix ? Sign up now":"Already have a account ? Sign in now"}</p>
         </form>
     </div>
   ) 
---
+++ src/utlis/validate.js
@@ -0,0 +1,20 @@
+export const checkValidData =(email, password)=> {
+
+    if(email==""){
+        return "Enter email address";
+    }
+    if(password==""){
+        return "Enter password";
+    }
+    const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+
+    if(!isEmailValid){
+        return "Email is not Valid."
+    }
+    if(!isPasswordValid){
+        return "Password is not Strong."
+    }
+
+    return null;
+}
